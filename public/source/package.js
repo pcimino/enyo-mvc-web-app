6953fe95b00cd24029790a69142f514649b46a47
Paul L. Cimino <pcimino@translunardesigns.com>
2013-04-24 19:24:25 -0400

    Setting up enyo app for basic content

---
+++ public/source/package.js
@@ -0,0 +1,13 @@
+enyo.depends(
+	// include support libraries
+	"$lib/layout",
+	"$lib/onyx",
+	// include application sources
+	"css",
+	"models",
+	"controllers",
+	"views",
+	"apps",
+	// include our default entry point
+	"start.js"
+);
