9f42f1f43ce4cdf2f1f36a610e6552bc9ea75a2d
Paul L. Cimino <pcimino@translunardesigns.com>
2013-06-04 08:10:12 -0400

    script cleanup

--- node-server-3.js
+++
@@ -1,8 +0,0 @@
-// http://stackoverflow.com/questions/6084360/node-js-as-a-simple-web-server
-var port = process.argv[2] || 8888;
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname + "/public")
-).listen(port);
-
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
