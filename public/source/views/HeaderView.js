d9707d40a0e0aa73dc0d968e253a3152a5bbed3a
Paul L. Cimino <pcimino@translunardesigns.com>
2013-06-06 19:24:25 -0400

    Better templating of header and footer

---
+++ public/source/views/HeaderView.js
@@ -0,0 +1,18 @@
+
+enyo.kind({
+  name: "Bootplate.HeaderView",
+  kind: "enyo.FittableRows",
+  id: 'headerView',
+  tag: 'header', // give it a specific html tag
+  classes: "onyx",
+  fit: true,
+  components: [
+    {name: 'headerContainer', kind: "FittableColumns", fit: true, classes: "fittable-sample-box fittable-sample-mtb fittable-sample-o",
+     components: [
+       {name:'headerLeftContent', kind: "Image", src: "img/translunar.png"},
+       {name:'headerCenterContent', content: "header-center", fit: true, classes: "fittable-sample-box fittable-sample-mlr fittable-sample-o"},
+       {name:'headerRightContent', content: "header-right", classes: "fittable-sample-box fittable-sample-mlr"}
+     ]}
+  ]
+});
+
