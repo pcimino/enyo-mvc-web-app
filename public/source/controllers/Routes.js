372740e05805055f115e64cc2650978d7abbe773
Paul L. Cimino <pcimino@translunardesigns.com>
2013-06-07 15:21:12 -0400

    Starting to set up router

--- public/source/controllers/Routes.js
+++ public/source/controllers/Routes.js
@@ -23,13 +23,16 @@ enyo.kind({
 	login: function () {
 		// Still need to figure out how this wiring works
         // BootPlate.LoginController.setRoute(this.current);
+    console.log("login router");
 	},
 	userSignup: function () {
 		// Still need to figure out how this wiring works
         // BootPlate.UserSignupController.setRoute(this.current);
+    console.log("userSignup router");
 	},
 	forgotPassword: function () {
 		// Still need to figure out how this wiring works
         // BootPlate.ForgotPassword.setRoute(this.current);
+    console.log("forgotPassword router");
 	}
 });
