c4ef1e4ce158d777285d1094a52aef9d203b912c
Paul L. Cimino <pcimino@translunardesigns.com>
2013-06-27 19:24:25 -0400

    Have a mock binding framework started

--- scripts/update-git-modules.bat
+++ scripts/update-git-modules.bat
@@ -5,5 +5,5 @@ REM Development is the default environment, showing how to do it explicitly
 set NODE_ENV=development
 
 cd %SERVER_HOME%
-git submodule update --init --recursive
+git submodule foreach git pull origin master
 pause
--- scripts/update-git-modules.sh
+++ scripts/update-git-modules.sh
@@ -3,4 +3,4 @@ export SERVER_HOME=..
 # Development is the default environment, showing how to do it explicitly
 export NODE_ENV=development 
 cd $SERVER_HOME 
-git submodule update --init --recursive
+git submodule foreach git pull origin master
