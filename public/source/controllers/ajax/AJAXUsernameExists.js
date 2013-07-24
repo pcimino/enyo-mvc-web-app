02229758a743f327ea58c69ae0e5df2b4877ed1f
Paul L. Cimino <pcimino@translunardesigns.com>
2013-07-24 19:24:25 -0400

    Started working on user signup flow

---
+++ public/source/controllers/ajax/AJAXUsernameExists.js
@@ -0,0 +1,23 @@
+
+enyo.kind({
+  name: 'AJAX.UsernameExists'
+  , kind: 'AJAX.Parent'
+  , method:'GET'
+  , rest:'/api/v1/user/username/exists'
+	, constructor: function (props) {
+      this.inherited(arguments);
+      // properties mapped to published attributes get set
+      console.log(this.fireEvent)
+  }
+	, processError: function(inSender, inResponse) {
+      console.log('AJAX.UsernameExists processError');
+      if (this.fireEvent) {
+        this.owner.bubble(this.fireEvent, {exists: true, message: 'Username exists.'});
+      }
+	}
+	, processResponse: function(inSender, inResponse) {
+      console.log('AJAX.UsernameExists processResponse ');
+    this.owner.bubble(this.fireEvent, {exists: false, message: 'Username is available.'});
+      console.log(JSON.stringify(inResponse, null, 2));
+  }
+});
