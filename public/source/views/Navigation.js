cc267300f241ca305f9509253ad510288839b15c
Paul L. Cimino <pcimino@translunardesigns.com>
2013-09-05 19:24:25 -0400

    Fixed Gravatar and link buttons

---
+++ public/source/views/Navigation.js
@@ -0,0 +1,32 @@
+// Parent for Navigation
+enyo.kind({
+  name: 'Bootplate.Navigation'
+  , kind: "enyo.Component"
+  , create: function() {
+      this.inherited(arguments);
+  }
+  /** Traditional buttons work in the MVC app, but not with the
+  *   user authentication check, somehow the mvcApp.controllers.routes.trigger({location:'/readUserList'});
+  *   doesn't so the exact same thing as <a href="#/readUserList">Read user List</a>
+  */
+  , createLinkButton: function(owner, link, text, addClass) {
+      var classList = "onyx-blue button-link";
+      if (addClass) {
+        classList = classList + " " + addClass;
+      }
+      owner.createComponent(
+        { kind: "onyx.Button"
+         , content: text
+         , classes: classList
+         , owner: owner
+         , tag: 'a'
+         , attributes: {
+           href: '#/' + link
+         }
+        });
+    }
+});
+
+
+
+
--- public/source/views/package.js
+++ public/source/views/package.js
@@ -1,6 +1,9 @@
 enyo.depends(
   "ParentContent.js"
   , "ParentView.js"
+  , "Navigation.js"
   , "auth"
   , "public"
 );
+
+
