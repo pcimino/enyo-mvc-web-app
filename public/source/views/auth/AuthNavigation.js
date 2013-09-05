cb568650f8a947427d42e7e5c594254031eff553
Paul L. Cimino <pcimino@translunardesigns.com>
2013-09-05 13:56:45 -0400

    Fixed Gravatar and link buttons

--- public/source/views/auth/AuthNavigation.js
+++ public/source/views/auth/AuthNavigation.js
@@ -6,27 +6,29 @@ enyo.kind({
       this.inherited(arguments);
   }
   , setupTopNav: function(owner) {
-    var topNavToolbar = owner.createComponent({kind: "onyx.Toolbar", name: 'topNavToolbar', owner: owner});
-    var adminNavToolbar = owner.createComponent({kind: "onyx.Toolbar", name: 'adminNavToolbar', owner: owner});
+      this.inherited(arguments);
+      var topNavToolbar = owner.createComponent({kind: "onyx.Toolbar", name: 'topNavToolbar', owner: owner});
+      var adminNavToolbar = owner.createComponent({kind: "onyx.Toolbar", name: 'adminNavToolbar', owner: owner});
 
-    // need to create buttons that behave as links
-    this.createLinkButton(topNavToolbar, 'home', 'Home');
-    this.createLinkButton(topNavToolbar, 'updateMyUserInfo', 'Update My Info');
-    this.createLinkButton(topNavToolbar, 'updateMyPassword', 'Update My Password');
-    this.createLinkButton(topNavToolbar, 'logout', 'Logout', 'button-float-right');
+      // need to create buttons that behave as links
+      this.createLinkButton(topNavToolbar, 'home', 'Home');
+      this.createLinkButton(topNavToolbar, 'updateMyUserInfo', 'Update My Info');
+      this.createLinkButton(topNavToolbar, 'updateMyPassword', 'Update My Password');
+      this.createLinkButton(topNavToolbar, 'logout', 'Logout', 'button-float-right');
 
-
-    /** Begin Admin navs, need a way to show/hide this*/
-    this.createLinkButton(adminNavToolbar, 'readUserInfo', 'Read User Info');
-    this.createLinkButton(adminNavToolbar, 'readUserList', 'Search For Users');
-    this.createLinkButton(adminNavToolbar, 'deleteUser', 'Delete User');
-
+      /** Begin Admin navs, need a way to show/hide this*/
+      this.createLinkButton(adminNavToolbar, 'readUserInfo', 'Read User Info');
+      this.createLinkButton(adminNavToolbar, 'readUserList', 'Search For Users');
+      this.createLinkButton(adminNavToolbar, 'deleteUser', 'Delete User');
   }
   , setupLeftNav: function(owner) {
+      this.inherited(arguments);
   }
   , setupRightNav: function(owner) {
+      this.inherited(arguments);
   }
   , setupBottomNav: function(owner) {
+      this.inherited(arguments);
   }
 });
 
