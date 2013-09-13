d64d003fc793294c2ada50a5abf68f9a00b7564b
Paul L. Cimino <pcimino@translunardesigns.com>
2013-09-13 12:33:56 -0400

    Starting on delete user flow

--- public/source/views/auth/body/DeleteUserContent.js
+++ public/source/views/auth/body/DeleteUserContent.js
@@ -1,3 +1,5 @@
+// http://macfja.github.io/enyo2-lib/onyx/dynamiclist.html
+
 /**
 * This DeleteUserContent kind, restricted to administrators
 *
@@ -9,30 +11,35 @@ enyo.ready(function() {
     , kind: "Bootplate.ParentContent"
     , id: 'deleteUserContent'
     , authFlag: true // used to help determine if user has access to this page
-    , bindings: [
-
-    ]
+    , published : {
+      listRef:''
+    }
     // This checks to see if the user is allowed on this page
     , rendered: function() {
         this.inherited(arguments);
+        if (!mvcApp.adminFlag) {
+          mvcApp.controllers.routes.trigger({location:'/home'});
+        }
+        // populate the list
     }
     , setupBodyContent: function(owner) {
         this.insertFormSpace(owner);
-        this.insertInternalLink(owner, 'readUserInfo', 'Read User Info');
-        this.insertBreak(owner);
-        this.insertInternalLink(owner, 'readUserList', 'Search For Users');
-        this.insertBreak(owner);
-        this.insertInternalLink(owner, 'updateUserInfo', 'Update User');
+        this.listRef = owner.createComponent({
+          name: "userList"
+          , style: "height: 250px; border: 1px solid; width:75%;margin-left:15%;"
+          , kind: "enyo.List"
+          , classes:"form-input-box form-top-margin"
+          , owner: owner
+          , handlers: {
 
-        this.insertBreak(owner);
-        this.insertInternalLink(owner, 'home', 'Home');
-        this.insertBreak(owner);
-        this.insertInternalLink(owner, 'logout', 'Logout');
+          }
+        });
 
         owner.render();
     } // end setupBodyContent
   });
 });
+
 
 
 
