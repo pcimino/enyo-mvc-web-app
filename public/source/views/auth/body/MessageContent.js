4a769301550c7804eeebeb19aa79177ad9d6163c
Paul L. Cimino <pcimino@translunardesigns.com>
2013-10-02 19:24:25 -0400

    Updated messaging

--- public/source/views/auth/body/MessageContent.js
+++ public/source/views/auth/body/MessageContent.js
@@ -26,37 +26,16 @@ enyo.ready(function() {
         this.inherited(arguments);
     }
     , setupBodyContent: function(owner) {
+        this.createComponent(
+          { name: "messagingDialog"
+            , kind: "Bootplate.MessagingDialog"
+            , owner: this
+          }
+        );
         this.createComponent({content:'Message Threads', style: "margin-left: 10%;margin-bottom: 10px;padding-top: 30px;", owner:this});
-        this.createComponent(
-          { name: "subject"
-            , kind: "onyx.Input"
-            , classes:"form-input-box "
-            , placeholder: "Subject"
-            , owner: this
-            , style: "width:10%; margin-left:10%; "
-          }
-        );
+
         this.insertBreak(owner);
-        this.createComponent(
-          { name: "message"
-            , kind: "onyx.Input"
-            , classes:"form-input-box "
-            , placeholder: "Message"
-            , owner: this
-            , style: "width:80%; margin-left:10%; "
-          }
-        );
-        this.insertBreak(owner);
-        this.createComponent(
-          { kind: "onyx.Button"
-            , content: "Send Message"
-            , classes: "onyx-blue "
-            , owner: this
-            , ontap: 'sendMessage'
-            , style: "margin-left:10%; "
-          }
-        );
-        this.insertBreak(this);
+
         this.insertBreak(this);
         this.createComponent({kind: enyo.Checkbox, name: 'showArchivedThreadsCheckbox', onActivate: 'threadCheckboxChanged', content:'Show Archived Message Threads', style: "margin-left: 10%;margin-bottom: 10px;", owner:this});
         this.createComponent({name: "messageThreadList"
@@ -87,20 +66,7 @@ enyo.ready(function() {
         this.getMessageThreadsUserScreen();
         this.getSystemMessagesUserScreen();
     } // end setupBodyContent
-
-    , sendMessage: function(inSender, inEvent) {
-        var subject = this.$.subject.getValue();
-        var message = this.$.message.getValue();
-        if (subject && subject.length > 0 && message && message.length > 0) {
-          // archive the system message
-          var ajaxSysMessage = new AJAX.SendSystemMessage({owner:this, fireEvent:'onGetSystemMessagesAdminScreen'});
-          ajaxSysMessage.makeRequest({subject: subject, message:message });
-        } else {
-          mvcApp.showErrorMessage('Missing Data', 'Enter the subject and message before sending.');
-        }
-    }
     , threadCheckboxChanged : function() {
-        console.log('Checkbox value ' + this.$.showArchivedThreadsCheckbox.getChecked());
         // reload the message list
         this.getMessageThreadsUserScreen();
     }
@@ -127,14 +93,12 @@ enyo.ready(function() {
       // build the rows of messages
       var messageContent = "";
       for (var i = 0; i < inEvent.context.messages.length; i++) {
-        var from = inEvent.context.fromUsername;
-        if ((i % 2) == 1) from = inEvent.context.toUsername;
         if (i > 0) messageContent = messageContent + ',';
         messageContent = messageContent + '{"kind":"FittableColumns","components": [';
         messageContent = messageContent + '{"content":"From: ","classes":"list-item-margin bold-text"}';
-        messageContent = messageContent + ',{"content":"' + from + '"}';
+        messageContent = messageContent + ',{"content":"' + inEvent.context.messages[i].from + '"}';
         messageContent = messageContent + ',{"content":"Message: ","classes":"list-item-margin bold-text"}';
-        messageContent = messageContent + ',{"content":"' + inEvent.context.messages[i] + '"}';
+        messageContent = messageContent + ',{"content":"' + inEvent.context.messages[i].message + '"}';
         messageContent = messageContent + ']}';
       }
 
@@ -163,14 +127,12 @@ enyo.ready(function() {
         //    inEvent.template="<div style=\"border: 2px solid #000; font-size: 20px; padding: 10px;\">" + inEvent.context.fromUsername + "</div>";
     }
     , messageThreadTap: function(inSender, inEvent) {
-        var objId = (inSender.id.substring(inSender.id.indexOf('replyMessageThread_') + ("replyMessageThread_").length)).trim();
-      console.log('messageThreadTap ' + objId + ":" + JSON.stringify(inEvent))
-    }
-    , archiveMessageThread: function(inSender, inEvent) {
-        var objId = (inSender.id.substring(inSender.id.indexOf('archiveMessageThread_') + ("archiveMessageThread_").length)).trim();
-        // archive the system message
-        var ajaxMessage = new AJAX.ArchiveMessageThread({owner:this, fireEvent:'onGetSystemMessagesUserScreen'});
-        ajaxMessage.makeRequest({messageThreadId: objId});
+        var messageThread = inSender.rows[inEvent.index].source;
+        console.log('messageThreadTap ', messageThread )
+
+        // show the message dialog
+        this.$.messagingDialog.setupDialog(messageThread);
+        this.$.messagingDialog.show();
     }
     /////////////////////
     // System messages
@@ -213,6 +175,8 @@ enyo.ready(function() {
     }
   });
 });
+
+
 
 
 
