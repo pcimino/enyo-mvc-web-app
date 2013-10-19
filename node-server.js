4afcf5ef71ac538bc1df86435a54a122017bdbf5
Paul L. Cimino <pcimino@translunardesigns.com>
2013-10-19 19:24:25 -0400

    Added a developer page

--- node-server.js
+++ node-server.js
@@ -2,14 +2,19 @@
 
 // http://erichonorez.wordpress.com/2013/02/04/a-basic-web-server-with-node-js-and-express/
 var port = process.argv[2] || 8888;
-var express = require('express'); 
-var app = express(); 
+var express = require('express');
+var app = express();
 
-app.configure(function () {     
-    app.use(         
-        "/", //the URL throught which you want to access to you static content         
-        express.static(__dirname + "/public") //where your static content is located in your filesystem     
-    ); 
-}); 
-app.listen(port); //the port you want to use 
+app.configure(function () {
+    app.get('/ping', function (req, res) {
+      var ip = req.headers['x-forwarded-for'] || req.headers['X-Real-IP'] || req.headers['X-Originating-IP'] || req.headers['X-Remote-IP']  || req.connection.remoteAddress;
+      res.send({'server':ip});
+    });
+
+    app.use(
+        "/", //the URL throught which you want to access to you static content
+        express.static(__dirname + "/public") //where your static content is located in your filesystem
+    );
+});
+app.listen(port); //the port you want to use
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
