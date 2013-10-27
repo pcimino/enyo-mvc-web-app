842fc412fd4a122907b272853d32eb338e345330
Paul L. Cimino <pcimino@translunardesigns.com>
2013-10-27 19:24:25 -0400

    Refactoring to be inline with enyo bootplate project

--- node-server.js
+++ node-server.js
@@ -13,7 +13,7 @@ app.configure(function () {
 
     app.use(
         "/", //the URL throught which you want to access to you static content
-        express.static(__dirname + "/public") //where your static content is located in your filesystem
+        express.static(__dirname + "/") //where your static content is located in your filesystem
     );
 });
 app.listen(port); //the port you want to use
